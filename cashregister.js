060a6b019ead97385ccecb7b5a43f182fb160565
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-20 18:06:22 -0600

    Add functionality for 'Insufficient Funds' and 'Closed'

--- cashregister.js
+++ cashregister.js
@@ -29,8 +29,20 @@
 // ]
 
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+  let changeDue = (cash - price).toFixed(2);
+  let single = cid.reduce((el1, el2) => el1.concat(el2))
+  let singleFilter = single.filter((elem) => {if(typeof(elem) === 'number'){return elem}})
+  let totalCID = singleFilter.reduce((x,y) => x+y, 0).toFixed(2);
+
+  if(totalCID < changeDue){
+    return {status: "INSUFFICIENT_FUNDS", change: []} 
+  } else if(totalCID === changeDue){
+    return {status: "CLOSED", change: [0]} // Fix the change drawer
+  } else {
+    return {status: "OPEN", change: [2]} // Fix the change drawer
+  }
+
+  // return changeDue;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
